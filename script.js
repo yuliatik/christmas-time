e70840c500e43df622b33e6682296b66e7a893e9
yuliatik <yulia.tik45@gmail.com>
2024-04-15 21:17:38 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,45 @@
+function christmasCountDown(){
+    const christmasDate = new Date("October 21, 2023 13:04");
+    const now = new Date();
+    const diff = christmasDate - now;
+    const msInSeconds = 1000;
+    const msInMinutes = 60*1000;
+    const msInHours = 60*60*1000;
+    const msInDays = 24*60*60*1000;
+
+    const displayDay = Math.floor(diff/msInDays);
+    document.querySelector(".days").textContent = displayDay;
+
+    const displayHour = Math.floor((diff%msInDays)/msInHours);
+    document.querySelector(".hours").textContent=displayHour;
+
+    const displayMinutes = Math.floor((diff%msInHours)/msInMinutes);
+    document.querySelector(".minutes").textContent = displayMinutes;;
+
+    const displaySeconds = Math.floor((diff%msInMinutes)/msInSeconds);
+    document.querySelector(".seconds").textContent = displaySeconds;
+    
+    if(diff<=0){
+        document.querySelector(".days").textContent = 0;
+        document.querySelector(".hours").textContent = 0;
+        document.querySelector(".minutes").textContent = 0;
+        document.querySelector(".seconds").textContent = 0;
+        clearInterval(timerID);
+        merryChristmas();
+    }
+
+    }
+
+   let timerID = setInterval(christmasCountDown, 1000);
+
+   function merryChristmas(){
+    const heading = document.querySelector("h1");
+    heading.textContent = "Weihnachtsmann ist da!!! Frohe Weihnachten!!!"
+    heading.classList.add("new_color");
+    body.style.backgroundImage = "url('home.jpeg.jpg')";
+   }
+
+   const button = document.querySelector("#myButton");
+   button.addEventListener("click", function(){
+   document.querySelector("#myAudio").play();
+   })
